50975b71101d4b699a8037456fee989813bca765
robiul26 <robiulrobi26@gmail.com>
2022-01-12 00:55:12 +0600

    links added

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -8,7 +8,7 @@ import {blog01,
     blog05} from './imports';
 const Blog = () => {
     return (
-        <div className='gpt3__blog section__padding'>
+        <div className='gpt3__blog section__padding' id='blog'>
             <div className="gpt3__blog-heading">
                 <h1 className='gradient__text'>A lot is happening,
                     We are blogging about it.</h1>
